ddbe60b31fed633ba0df38107f941fb38ba605b8
ben <gomben397@hengersor.hu>
2024-12-10 12:24:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-btn.onclick function(){
+btn.onclick = function(){
     alert("hello")
 }
 
 btn.innerHTML = "haiii"
+
+document.getElementById("native-button-container").appendChild(btn);
+
+//React
+
+const btn = React.createElement("button",
+    {
+        onclick: function(){
+            alert("Click event occurred")
+        },
+    },
+    "React"
+)
+
+ReactDOM.render(btn, document.getElementById("react-button-container"));
+
+ReactDOM.render(
+    <button
+        onClick={()=>{
+            alert("Click event occurred");
+        },
+        className="asd"
+    }/>)
